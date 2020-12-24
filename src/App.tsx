7d65418d8a5f5ecbc5b29d5a0851e20d2d5411ea
Daniel Schmitz <danieljfa@gmail.com>
2020-12-24 12:01:39 -0300

    set stte example with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HelloWorld from './components/HelloWorld';
+import UserHookExample from './components/UserHookExample';
 
 function App() {
   return (
     <div>
       <HelloWorld name="Daniel"/>
+      <UserHookExample />
     </div>
   );
 }
---
+++ src/components/UserHookExample.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+type User = {
+    name: string
+    email: string
+}
+
+
+const UserHookExample: React.FC = () => {
+    const [user, setUser] = useState<User|null>(null)
+
+    function handleButtonClick() {
+        setUser({name:'joe',email:'joe@gmail.com'})
+    }
+
+    return (<div>
+        user: {user?.name ?? 'null'} email: {user?.email ?? 'null'}
+        <br />
+        <button onClick={() => handleButtonClick()}>Set State</button>
+    </div>)
+}
+
+export default UserHookExample;
