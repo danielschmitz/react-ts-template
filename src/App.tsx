6bec1e2935483f0f0d55a25c22cf277675d3f927
Daniel Schmitz <danieljfa@gmail.com>
2020-12-24 11:46:28 -0300

    Exemplo de componente TS com props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div>
-      Hello World
+      <HelloWorld name="Daniel"/>
     </div>
   );
 }
